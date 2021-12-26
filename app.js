82b0726ba16b587e660ea51466dc4e8d3f886540
Olcay Kaplan <olcay.kaplan@datassist.com.tr>
2021-12-26 20:27:26 +0300

    cleanBlog second homework

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-/*
-CleanBlog proje klasörünü oluşturalım.
-Package.json dosyasını oluşturalım.
-Prettier ayarlarını yapalım.(İsteğe bağlı)
-Express ve Nodemon modüllerini indirelim.
-git init ile lokal repomuzu oluşturalım.
-get request içerisinde const blog = { id: 1, title: "Blog title", description: "Blog description" }, içeriğini gönderelim.
-.gitignore dosyası oluşturalım ve ilk repomuzu gönderelim.
+/*  2. Ödev
+Public klasörü oluşturup statik dosyalarımızı içerisine yerleştirelim.
+İlgili middleware fonksiyonunu yazarak public klasörümüzü uygulamamıza kaydedelim.
+EJS modülünü indirelim.
+Uygulamamızda EJS modülünü kullanacağımızı belirtelim.
+Views klasörü oluşturalım ve tüm .html dosyalarımız views klasörü içerisinde .ejs dosyalarına çevirelim.
+Partials klasör yapısını oluşturalım.
+İlgili yönlendirmeleri ve _navigation.ejs klasöründeki link düzenlemelerini yapalım.
 */
 
-const bodyParser = require('body-parser');
 const express = require('express');
-
+const ejs = require('ejs');
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs")
 
-app.get('/', (req,res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" };
-    res.status(200).send(blog)
-})
+// Middleware
+app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Port is working on ${PORT} successfully.`)
-})
+  console.log(`Port is working on ${PORT} successfully.`);
+});
